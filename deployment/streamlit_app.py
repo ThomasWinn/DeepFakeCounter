fa8f94d82abcbbb72075455308c49129c5510bbe
Thomas Nguyen <tpnguyen1999@gmail.com>
2023-10-26 17:33:33 -0500

    feat: added file upload to streamlit

--- deployment/streamlit_app.py
+++ deployment/streamlit_app.py
@@ -1,10 +1,13 @@
+
+from PIL import Image
 import streamlit as st
 
-st.title('This is ThomasWinn"s DeepFakeCounter Project :sunglasses:')
+st.title("This is ThomasWinn's DeepFakeCounter Project :sunglasses:")
 
 uploaded_files = st.file_uploader("Please upload a single / multiple picture file", accept_multiple_files=True)
 if uploaded_files is not None:
     for uploaded_file in uploaded_files:
-        bytes_data = uploaded_file.read()
-        st.write('filename:', uploaded_file.name)
-        st.write(bytes_data)
+        image = Image.open(uploaded_file)
+        print(image.shape)
+        st.image(uploaded_file)
+        st.divider()
