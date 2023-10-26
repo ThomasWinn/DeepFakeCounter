aad615f5b01e20e758727dd58d4959f4ac356ab8
Thomas Nguyen <tpnguyen1999@gmail.com>
2023-10-26 17:38:09 -0500

    refactor: checking if shape needs paraenthese or not

--- deployment/streamlit_app.py
+++ deployment/streamlit_app.py
@@ -4,11 +4,11 @@ import streamlit as st
 st.title("This is ThomasWinn's DeepFakeCounter Project :sunglasses:")
 
 uploaded_files = st.file_uploader(
-    "Please upload a single / multiple picture file", accept_multiple_files=True
+    "Please upload a single / multiple picture file", accept_multiple_files=False
 )
 if uploaded_files is not None:
     for uploaded_file in uploaded_files:
         image = Image.open(uploaded_file)
-        print(image.shape)
+        print(image.shape())
         st.image(uploaded_file)
         st.divider()
